b242ddd833d72b7bb46f0255926e6f193817a42e
felip1510 <felipe.marinhodeandrade08@gmail.com>
2026-07-01 21:04:03 -0300

    Add files via upload

---
+++ projeto-industrial/main.py
@@ -0,0 +1,8 @@
+#main - entrada principal do sistema
+#inica o sistema
+#chama funcoes
+#contem o escopo geral do projeto (menu)
+
+from funcionario import listar_funcionarios
+
+listar_funcionarios()
