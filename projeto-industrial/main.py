f217a382a95513661dea84dca7fe831ed5bac67c
felip1510 <felipe.marinhodeandrade08@gmail.com>
2026-07-01 21:34:42 -0300

    Delete projeto-industrial directory

--- projeto-industrial/main.py
+++
@@ -1,8 +0,0 @@
-#main - entrada principal do sistema
-#inica o sistema
-#chama funcoes
-#contem o escopo geral do projeto (menu)
-
-from funcionario import listar_funcionarios
-
-listar_funcionarios()
